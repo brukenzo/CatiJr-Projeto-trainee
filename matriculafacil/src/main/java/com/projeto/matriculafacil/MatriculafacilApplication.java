bcfb307c8dd38679872abf658513886aa9f0d458
Bruno Kenzo <brunokenzosaito@gmail.com>
2026-06-13 15:30:29 -0300

    chore: configuração inicial do spring boot

---
+++ matriculafacil/src/main/java/com/projeto/matriculafacil/MatriculafacilApplication.java
@@ -0,0 +1,13 @@
+package com.projeto.matriculafacil;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MatriculafacilApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MatriculafacilApplication.class, args);
+	}
+
+}
---
+++ matriculafacil/src/test/java/com/projeto/matriculafacil/MatriculafacilApplicationTests.java
@@ -0,0 +1,13 @@
+package com.projeto.matriculafacil;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MatriculafacilApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
