03012e2e3140b29fd4a2f9e4d08150117065def9
Bruno Kenzo <brunokenzosaito@gmail.com>
2026-07-24 01:36:19 -0300

    feat: adiciona anotações de validação no AlunoCadastroDto

--- backend/src/main/java/com/projeto/matriculafacil/dto/AlunoCadastroDto.java
+++ backend/src/main/java/com/projeto/matriculafacil/dto/AlunoCadastroDto.java
@@ -1,3 +1,18 @@
 package com.projeto.matriculafacil.dto;
 
-public record AlunoCadastroDto (String nome, String email, String senha) {}
+import jakarta.validation.constraints.Email;
+import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.Size;
+
+public record AlunoCadastroDto (
+    @NotBlank(message = "O nome é obrigatório")
+    String nome,
+
+    @NotBlank(message = "O email é obrigatório")
+    @Email(message = "O email é inválido")
+    String email,
+
+    @NotBlank(message = "A senha é obrigatória")
+    @Size(min = 6, message = "A senha deve ter pelo menos 6 catacteres")
+    String senha
+) {}
