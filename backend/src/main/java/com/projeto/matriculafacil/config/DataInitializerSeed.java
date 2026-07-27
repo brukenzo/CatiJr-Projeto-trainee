c89b6e81efeb20248e14d8290531a912c4582841
Bruno Kenzo <brunokenzosaito@gmail.com>
2026-07-27 18:25:24 -0300

    test: adiciona mais matérias ao seed para teste

--- backend/src/main/java/com/projeto/matriculafacil/config/DataInitializerSeed.java
+++ backend/src/main/java/com/projeto/matriculafacil/config/DataInitializerSeed.java
@@ -66,7 +66,7 @@ public class DataInitializerSeed implements CommandLineRunner{
             calc3.setDisponivel(true);
             this.materiaRepository.save(calc3);
 
-            // AED 1 
+            // Algoritmo e estrutura de dados 1
             var aed1 = new MateriaModel();
             aed1.setCodigoMateria("COMP101");
             aed1.setNome("Algoritmos e Estrutura de Dados 1");
@@ -92,7 +92,7 @@ public class DataInitializerSeed implements CommandLineRunner{
             fis1.setDisponivel(true);
             this.materiaRepository.save(fis1);
     
-            // IPA
+            // Introdução ao pensamento algorítmico
             var ipa = new MateriaModel();
             ipa.setCodigoMateria("COMP110");
             ipa.setNome("Introdução ao Pensamento Algorítmico");
@@ -105,7 +105,7 @@ public class DataInitializerSeed implements CommandLineRunner{
             ipa.setDisponivel(true);
             this.materiaRepository.save(ipa);
 
-            // LD
+            // Lógica digital
             var ld = new MateriaModel();
             ld.setCodigoMateria("COMP120");
             ld.setNome("Arquitetura e Organização de Computadores");
@@ -117,17 +117,151 @@ public class DataInitializerSeed implements CommandLineRunner{
             ld.setDescricao("Álgebra booleana e verilog");
             ld.setDisponivel(false);
             this.materiaRepository.save(ld);
+
+            // Geometria analítica
+            var ga = new MateriaModel();
+            ga.setCodigoMateria("MAT110");
+            ga.setNome("Geometria Analítica");
+            ga.setCredito(4);
+            ga.setQtdVagas(40);
+            ga.setHorario("Qua 08:00 - 12:00");
+            ga.setPreRequisito("NENHUM");
+            ga.setProfessor("Cláudia");
+            ga.setDescricao("Vetores, matrizes, sistemas lineares e transformações lineares.");
+            ga.setDisponivel(true);
+            this.materiaRepository.save(ga);
+
+            // Matemática discreta
+            var md = new MateriaModel();
+            md.setCodigoMateria("MAT120");
+            md.setNome("Matemática Discreta");
+            md.setCredito(4);
+            md.setQtdVagas(35);
+            md.setHorario("Seg 14:00 - 16:00, Qua 14:00 - 16:00");
+            md.setPreRequisito("NENHUM");
+            md.setProfessor("Homem");
+            md.setDescricao("Lógica proposicional, teoria dos conjuntos, indução matemática e combinatória.");
+            md.setDisponivel(true);
+            this.materiaRepository.save(md);
+
+            // Equações diferenciais ordinárias
+            var edo = new MateriaModel();
+            edo.setCodigoMateria("MAT130");
+            edo.setNome("Equações Diferenciais Ordinárias");
+            edo.setCredito(4);
+            edo.setQtdVagas(30);
+            edo.setHorario("Ter 19:00 - 21:00, Qui 19:00 - 21:00");
+            edo.setPreRequisito("MAT102");
+            edo.setProfessor("Roberto");
+            edo.setDescricao("Equações de 1ª e 2ª ordem, transformada de Laplace e sistemas lineares.");
+            edo.setDisponivel(true);
+            this.materiaRepository.save(edo);
+
+            // Física 2
+            var fis2 = new MateriaModel();
+            fis2.setCodigoMateria("FIS102");
+            fis2.setNome("Física 2");
+            fis2.setCredito(4);
+            fis2.setQtdVagas(30);
+            fis2.setHorario("Seg 19:00 - 21:00, Qua 19:00 - 21:00");
+            fis2.setPreRequisito("FIS101");
+            fis2.setProfessor("Maria");
+            fis2.setDescricao("Oscilações, ondas mecânicas, fluidos e termodinâmica.");
+            fis2.setDisponivel(true);
+            this.materiaRepository.save(fis2);
+
+            // Física 3 (Eletromagnetismo)
+            var fis3 = new MateriaModel();
+            fis3.setCodigoMateria("FIS103");
+            fis3.setNome("Física 3");
+            fis3.setCredito(4);
+            fis3.setQtdVagas(25);
+            fis3.setHorario("Ter 10:00 - 12:00, Qui 10:00 - 12:00");
+            fis3.setPreRequisito("FIS102");
+            fis3.setProfessor("Cláudia");
+            fis3.setDescricao("Lei de Coulomb, campo elétrico, potencial, magnetismo e leis de Maxwell.");
+            fis3.setDisponivel(true);
+            this.materiaRepository.save(fis3);
+
+            // AED 2
+            var aed2 = new MateriaModel();
+            aed2.setCodigoMateria("COMP102");
+            aed2.setNome("Algoritmos e Estrutura de Dados 2");
+            aed2.setCredito(6);
+            aed2.setQtdVagas(30);
+            aed2.setHorario("Ter 14:00 - 16:00, Qui 14:00 - 16:00, Sex 21:00 - 23:00");
+            aed2.setPreRequisito("COMP101");
+            aed2.setProfessor("Mario");
+            aed2.setDescricao("Árvores balanceadas, tabelas hash, heaps e complexidade de algoritmos.");
+            aed2.setDisponivel(true);
+            this.materiaRepository.save(aed2);
+
+            // Programação Orientada a Objetos
+            var poo = new MateriaModel();
+            poo.setCodigoMateria("COMP130");
+            poo.setNome("Programação Orientada a Objetos");
+            poo.setCredito(4);
+            poo.setQtdVagas(35);
+            poo.setHorario("Seg 08:00 - 12:00");
+            poo.setPreRequisito("COMP101");
+            poo.setProfessor("Bueno");
+            poo.setDescricao("Classes, encapsulamento, herança, polimorfismo e padrões de projeto em Java.");
+            poo.setDisponivel(false);
+            this.materiaRepository.save(poo);
+
+            // Teoria dos Grafos
+            var grafos = new MateriaModel();
+            grafos.setCodigoMateria("COMP104");
+            grafos.setNome("Teoria dos Grafos");
+            grafos.setCredito(4);
+            grafos.setQtdVagas(30);
+            grafos.setHorario("Qua 10:00 - 12:00, Sex 10:00 - 12:00");
+            grafos.setPreRequisito("COMP102");
+            grafos.setProfessor("Daniela");
+            grafos.setDescricao("Busca em largura e profundidade, caminhos mínimos, árvores geradoras e fluxo.");
+            grafos.setDisponivel(false);
+            this.materiaRepository.save(grafos);
+
+            // Sistemas Operacionais
+            var so = new MateriaModel();
+            so.setCodigoMateria("COMP105");
+            so.setNome("Sistemas Operacionais");
+            so.setCredito(6);
+            so.setQtdVagas(25);
+            so.setHorario("Seg 14:00 - 18:00, Qua 16:00 - 18:00");
+            so.setPreRequisito("COMP120");
+            so.setProfessor("Helio");
+            so.setDescricao("Gerenciamento de processos, threads, concorrência, memória virtual e sistemas de arquivos.");
+            so.setDisponivel(true);
+            this.materiaRepository.save(so);
+
+            // Inteligência Artificial
+            var ia = new MateriaModel();
+            ia.setCodigoMateria("COMP106");
+            ia.setNome("Inteligência Artificial");
+            ia.setCredito(4);
+            ia.setQtdVagas(40);
+            ia.setHorario("Sex 14:00 - 18:00");
+            ia.setPreRequisito("MAT110");
+            ia.setProfessor("Helena");
+            ia.setDescricao("Agentes autônomos, busca heurística, raciocínio probabilístico e introdução ao aprendizado de máquina.");
+            ia.setDisponivel(false);
+            this.materiaRepository.save(ia);
         }
         // Cria o aluno com email "bruno@teste.com"
         var alunoExistente = alunoRepository.findByEmail("bruno@teste.com");
         if (!alunoExistente.isPresent()) {
             MateriaModel[] materiaConcluir = {
                 materiaRepository.findByCodigoMateria("MAT101").orElseThrow(),
-                materiaRepository.findByCodigoMateria("COMP110").orElseThrow()
+                materiaRepository.findByCodigoMateria("COMP110").orElseThrow(),
+                materiaRepository.findByCodigoMateria("MAT102").orElseThrow(),
+                materiaRepository.findByCodigoMateria("FIS101").orElseThrow(),
+                materiaRepository.findByCodigoMateria("FIS102").orElseThrow(),
             };
             MateriaModel[] materiaReprovar = {
-                materiaRepository.findByCodigoMateria("FIS101").orElseThrow(),
-                materiaRepository.findByCodigoMateria("COMP101").orElseThrow()
+                materiaRepository.findByCodigoMateria("MAT120").orElseThrow(),
+                materiaRepository.findByCodigoMateria("COMP101").orElseThrow(),
+                materiaRepository.findByCodigoMateria("FIS103").orElseThrow()
             };
             criaAlunoExemplo(materiaConcluir, materiaReprovar);
         }
